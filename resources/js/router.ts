09855b6ac9a853db9a868d6278d570afa8b847f6
Marcus Coleman <Sunhat@users.noreply.github.com>
2019-09-25 15:22:17 +0100

    Login.vue set to index route

--- resources/js/router.ts
+++ resources/js/router.ts
@@ -1,16 +1,16 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
-import IndexPage from "./views/Index.vue"
+import LoginPage from "./components/Views/Auth/Login.vue"
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'index',
-            component: IndexPage
-        }
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: LoginPage
+    }
+  ]
 })
